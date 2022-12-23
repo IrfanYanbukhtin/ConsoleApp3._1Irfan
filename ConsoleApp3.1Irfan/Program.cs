fce18ff192b0ee420abd95784edb32705d8f046e
Yanbukhtin Irfan <IrfanfY@code.local>
2022-12-23 18:54:11 +0400

    mesaj

---
+++ ConsoleApp3.1Irfan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3._1Irfan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 1000000)
+                {
+                if (i % 3 == 0)
+                    Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
